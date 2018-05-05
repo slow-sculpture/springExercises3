a096fa29e21b490dd43c653267fe99649eb6261f
ChristianR <christian.rogowski@yahoo.pl>
2018-05-05 13:13:27 +0200

    Exercise 2 ->Spring Web MVC

--- src/main/java/pl/sda/spring/springExercise2/web/GithubRepoController.java
+++ src/main/java/pl/sda/spring/springExercise2/web/GithubRepoController.java
@@ -1,6 +1,8 @@
 package pl.sda.spring.springExercise2.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +20,9 @@ public class GithubRepoController {
 
     //adnotacja sprawiajaca ze mamy GET
     @GetMapping("/getRepo/{user}/{repositoryName}")
-    public String getRepositoryByUserAndRepo(@PathVariable("user") String user,
-                                             @PathVariable("repositoryName") String repositoryName) {
+    public ResponseEntity<String> getRepositoryByUserAndRepo(@PathVariable("user") String user,
+                                                            @PathVariable("repositoryName") String repositoryName) {
         String response = githubRepoService.getRepoByUserAndRepoName(user, repositoryName);
-        return response;
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
