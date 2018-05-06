95fb4eda861efe170350aca9be5225de9307e50a
ChristianR <christian.rogowski@yahoo.pl>
2018-05-06 13:32:54 +0200

    integration test for controller

--- src/test/java/pl/sda/spring/springExercise2/web/GithubRepoControllerITest.java
+++ src/test/java/pl/sda/spring/springExercise2/web/GithubRepoControllerITest.java
@@ -17,12 +17,16 @@ import pl.sda.spring.springExercise2.SpringExercise2Application;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringExercise2Application.class)
 @AutoConfigureMockMvc
 public class GithubRepoControllerITest {
+    private static String url = "/getRepo/{user}/{repositoryName}";
+    private static String user = "slow-sculpture";
+    private static String repo = "java-db";
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,8 +35,15 @@ public class GithubRepoControllerITest {
     public void shouldReturnValidResponse() throws Exception{
         //perform - jakie chcemy miec zapytanie
         // wez (get), wypisz na konsole(print) i parametry spodziewanego wyniku (andExpect)
-        mockMvc.perform(get("/getRepo/{user}/{repositoryName}",
-                "slow-sculpture", "java-db")).andDo(print())
-                .andExpect(status().isOk());
+        mockMvc.perform(get(url, user, repo)).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.owner.login").value(user));
+    }
+
+    @Test
+    public void shouldReturnCommitsByUserAndRepo() throws Exception{
+        mockMvc.perform(get(url+"/commits",user,repo))
+        .andDo(print())
+        .andExpect(status().isOk());
     }
 }
